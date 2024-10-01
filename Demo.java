8de9a71344fefaa33666a34afc913b444d9ee0d3
almastazeen <almastazeen@gmail.com>
2024-10-01 16:47:50 +0530

    second push

---
+++ Demo.java
@@ -0,0 +1,41 @@
+public class Demo{
+
+     public static void main(String[] args){
+     
+          byte age = 12;
+          byte continents = 7;
+	  byte subject = 6; 
+	  byte hoursinaday = 24;
+	  byte months = 12;
+
+       	  short distance =1000;
+	  short vehicalsinacompany =2000;
+	  short heightofmountain =1000;
+	  short pizzas = 1000;
+	  short momos = 1000;
+
+	  int speed =3456787;
+	  int area =-2546377;
+	  int length =-485463;
+	  int size =643638;
+	  int angle =647360;
+          
+	  
+ 	  System.out.println(speed);
+	  System.out.println(area);
+          System.out.println(length);
+          System.out.println(size);
+          System.out.println(angle);
+          System.out.println(age);
+	  System.out.println(continents);
+          System.out.println(subject);
+          System.out.println(hoursinaday);
+          System.out.println(months); 
+          System.out.println(distance);
+	  System.out.println(vehicalsinacompany);
+          System.out.println(heightofmountain);
+          System.out.println(pizzas);
+          System.out.println(momos);     
+     }
+}
+
