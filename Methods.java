1ae98c6a67e23d5965191cf993f1c0d255b5e7d4
almastazeen <almastazeen@gmail.com>
2024-10-01 16:45:10 +0530

    test push

---
+++ Methods.java
@@ -0,0 +1,20 @@
+public class Methods{
+
+    public static void main(String[] args){
+    	mul(2,5);
+    	mul(4,2);
+    	wificall(784512963);
+    
+    }
+
+    public static void mul(int number1, int number2){
+    	
+    	int mulValue = number1 * number2;
+    	System.out.println(mulValue);
+    }
+   
+   public static void wificall (int phonenumber){
+   	System.out.println("calling " + phonenumber);
+   }
+
+}
